010bfe3e15a0385cdd751dc9228109bbc6eed3c6
zia0001 <https://github.com/zia0001>
2024-11-20 01:33:23 +0500

    user registration completed

--- server/controllers/auth.js
+++ server/controllers/auth.js
@@ -1,3 +1,40 @@
 import bcrypt from "bcrypt";   // for password encryption 
 import jwt from "jsonwebtoken";
 import User from "../models/user.js";
+
+/* Register USER */
+export const register = async (req, res) => {
+  try {
+    const {
+      firstName,
+      LastName,
+      email,
+      password,
+      picturePath,
+      friends,
+      location,
+      occupation
+    } = req.body;       // destructoring these parameters from request body means from frontend we have send an object having these params
+    const salt = await bcrypt.genSalt(); // for password encryption so that the password does not get exposed
+    const passwordHash = await bcrypt.hash(password, salt);
+
+    const newUser = new User({
+      firstName,
+      LastName,
+      email,
+      password: passwordHash,
+      picturePath,
+      friends,
+      location,
+      occupation,
+      viewedProfile: Math.floor(Math.random() * 10000),
+      impressions: Math.floor(Math.random() * 10000)
+    });
+    const savedUser = await newUser.save();
+    res.status(201).json(savedUser);
+  }
+  catch (err) {
+    res.status(500).json({ error: err.message });
+  }
+
+}
