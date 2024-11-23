54b74282ac852f73554e27debf0a0bf0ff25ebf2
zia0001 <https://github.com/zia0001>
2024-11-23 22:29:22 +0500

    loggin in setup

--- server/controllers/auth.js
+++ server/controllers/auth.js
@@ -37,4 +37,22 @@ export const register = async (req, res) => {
     res.status(500).json({ error: err.message });
   }
 
-}
+};
+
+/* LOGGING IN */
+export const login = async (req, res) => {
+  try {
+    const { email, password } = req.body;
+    const user = await User.findOne({ email: email });
+    if (!user) return res.status(400).json({ msg: "User does not exist.n" });
+
+    const isMAtch = await bcrypt.compare(password, user.password);
+    if (!isMAtch) return res.status(400).json({ msg: "Invalid credentials." });
+
+    const token = jwt.sign({ id: user._id }, process.env.JWT_SECRET);
+    delete user.password;   // so that it does sent back to front-end
+    res.status(200).json({ token, user });
+  } catch (err) {
+    res.status(500).json({ error: err.message });
+  }
+};
